01f716eef35a4912bfa65cc9ee3e575a83aff5ac
galaparadi <gala_koyin@yahoo.co.id>
2022-04-28 00:20:16 +0700

    fix and refactor cron provider

--- src/lib/cron-provider.js
+++ src/lib/cron-provider.js
@@ -2,16 +2,19 @@ const fs = require('node:fs');
 const path = require('path');
 
 const cronProvider = fs.readdirSync(path.join(__dirname, '..', '/cron'))
-    .map(location => require(path.join(__dirname, '..', '/cron', location)))
-    .reduce((acc, item) => {
-        acc = acc.length ? [Promise.resolve(acc)] : [acc]
-        item = item.length ? [Promise.resolve(item)] : [item]
-        return [...acc, ...item];
-    });
+    .map(location => {
+        const task = require(path.join(__dirname, '..', '/cron', location));
+        return task // return task: Promise<object> || Promise<[object]>
+    }).filter(item => {
+        //TODO: check if promise
+        return item.then; //check if item is Promise
+    })
 
 async function reduce(cronProvider) {
     const row = await Promise.all(cronProvider);
-    return row.reduce((acc, item) => [...acc, ...item]);
+    return row
+        .map(item => item.length ? item : [item])
+        .reduce((acc, item) => [...acc, ...item]);
 }
 
 module.exports = reduce(cronProvider)
