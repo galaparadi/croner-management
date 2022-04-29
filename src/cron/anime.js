79c37f139ce2857c4d28b3295085395bd5ec0e3b
galaparadi <gala_koyin@yahoo.co.id>
2022-04-29 09:44:39 +0700

    implement logger

--- src/cron/anime.js
+++ src/cron/anime.js
@@ -1,26 +1,31 @@
 const { initDb, ScheduleModel } = require('../datasource/anime-schedule');
 const { anime: animeApi } = require('../datasource/anime');
+const logger = require('../logger/logger');
 
 const anime = (instance) => {
     return async function (self) {
-        // const axios = require('axios');
-        // axios.post('http://localhost:1111/emb', params.toString()).then(res => console.log(res.data)).catch(err => console.log(err));
-        let detail = await animeApi(instance.id);
-        const axios = require('axios');
-        const url = require('url');
-        const params = new url.URLSearchParams({
-            key: 'secret',
-            message: 'minutely',
-            animeId: instance.id
-        });
+        try {
+            // const axios = require('axios');
+            // axios.post('http://localhost:1111/emb', params.toString()).then(res => console.log(res.data)).catch(err => console.log(err));
+            let detail = await animeApi(instance.id);
+            const axios = require('axios');
+            const url = require('url');
+            const params = new url.URLSearchParams({
+                key: 'secret',
+                message: 'minutely',
+                animeId: instance.id
+            });
 
-        axios.post('http://localhost:1111/anime', params.toString())
-            .then(res => console.log(`success sending message : ${detail.title} - ${instance.id}`))
+            axios.post('http://localhost:1111/anime', params.toString())
+                .then(res => logger.info(`success sending message : ${detail.title} - ${instance.id}`))
 
-        if (!detail.airing) {
-            await instance.destroy()
-            self.stop();
-            console.log(`${detail.title} is ended`)
+            if (!detail.airing) {
+                await instance.destroy()
+                self.stop();
+                logger.info(`${detail.title} is ended`)
+            }
+        } catch (error) {
+            logger.error(error);
         }
     }
 }
@@ -29,7 +34,7 @@ const anime = (instance) => {
 module.exports = (async () => {
     const animeScheduleModel = await ScheduleModel(await initDb());
     const animeScheduleInstances = await animeScheduleModel.findAll();
-    animeScheduleInstances.forEach(item => console.log(`cron ${item.cron}, id ${item.id}`))
+    animeScheduleInstances.forEach(item => logger.info(`adding : cron ${item.cron}, id ${item.id}`))
     return animeScheduleInstances.map(animeScheduleInstance => Object.assign({}, {
         cron: animeScheduleInstance.cron, handler: anime(animeScheduleInstance)
     }))
