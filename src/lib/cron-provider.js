cfd1999cf43a2f6cde7f7623ea59eb1ca10ede44
galaparadi <gala_koyin@yahoo.co.id>
2023-01-25 23:26:28 +0700

    FIX : error when cron return 0 size promises

--- src/lib/cron-provider.js
+++ src/lib/cron-provider.js
@@ -6,14 +6,15 @@ const cronProvider = fs.readdirSync(path.join(__dirname, '..', '/cron'))
         const task = require(path.join(__dirname, '..', '/cron', location));
         return task // return task: Promise<object> || Promise<[object]>
     }).filter(item => {
-        //TODO: check if promise
         return item.then; //check if item is Promise
     })
 
 async function reduce(cronProvider) {
-    const row = await Promise.all(cronProvider);
+    const row = await Promise.allSettled(cronProvider);
     return row
-        .map(item => item.length ? item : [item])
+        .filter(item => item.value)
+        .map(item => item.value)
+        .map(item => Array.isArray(item) ? item : [item])
         .reduce((acc, item) => [...acc, ...item]);
 }
 
